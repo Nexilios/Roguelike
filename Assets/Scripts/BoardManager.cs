76227f74485b67a40cbe0120eb41a306ef57d7d3
Nicholas R. <nicho.reynaldo.work@gmail.com>
2025-09-29 21:45:31 +0700

    Board Data

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,6 +3,14 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Passable;
+    }
+    
+    [SerializeField]
+    private CellData[,] m_BoardData;
+    
     [SerializeField]
     private Tilemap m_Tilemap;
 
@@ -15,20 +23,25 @@ public class BoardManager : MonoBehaviour
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        
+        m_BoardData = new CellData[Width, Height];
 
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width; x++)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
                 }
                 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
